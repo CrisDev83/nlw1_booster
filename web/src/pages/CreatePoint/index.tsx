a8418b1668ddc918e89027d09e85f2152070b7de
CrisDev83 <cristhian.dev83@gmail.com>
2022-10-03 15:48:49 -0300

    map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
-//import {Map, TileLayer, Marker } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
+import axios from 'axios';
 
 import "./styles.css";
 
@@ -11,11 +13,85 @@ import baterias from '../../assets/baterias.svg';
 import lampadas from '../../assets/lampadas.svg';
 import organicos from '../../assets/organicos.svg';
 import papeis from '../../assets/papeis.svg';
-import eletronicos from '../../assets/eletronicos.svg';
+import eletronicos from '../../assets/eletronicos.svg'; 
 
+
+interface UF {
+  sigla: string;
+}
+
+interface City {
+  nome: string;
+}
 
 const CreatePoint = () => {
- 
+
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: '',
+  });
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+  const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+      navigator.geolocation.getCurrentPosition(position => {
+        const {latitude, longitude} = position.coords;
+
+        setInitialPosition([latitude, longitude])
+      })
+  }, []);
+
+  useEffect(() => {
+    axios.get<UF[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+      const ufInitials = response.data.map(uf => uf.sigla);
+      setUfs(ufInitials)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (selectedUf === '0') {
+      return;
+    }
+    axios.get<City[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+      const cityNames = response.data.map(city => city.nome);
+      setCities(cityNames)
+    })
+  }, [selectedUf])
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+    const uf = event.target.value;
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
+    const city = event.target.value;
+    setSelectedCity(city);
+  }
+
+  function handleMapClick(event: LeafletMouseEvent){
+   setSelectedPosition([
+    event.latlng.lat,
+    event.latlng.lng,
+   ])
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+    const {name, value} = event.target;
+    setFormData({...formData, [name]: event})
+  }
+
+  function handleSelectItem(){
+    console.log('teste')
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -34,17 +110,17 @@ const CreatePoint = () => {
           </legend>
           <div className="field">
             <label htmlFor="name">Nome da entidade</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" onChange={handleInputChange} required/>
           </div>
           <div className="field-group">
             <div className="field">
               <label htmlFor="email">E-mail</label>
-              <input type="email" name="email" />
+              <input type="email" name="email" onChange={handleInputChange} required/>
             </div>
 
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" name="whatsapp"/>
+              <input type="text" name="whatsapp" onChange={handleInputChange} required/>
             </div>
           </div>
         </fieldset>
@@ -54,18 +130,32 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+            <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={selectedPosition}/>  
+            </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado</label>
-              <select name="uf">
-                <option value="">Selecione o Estado</option>
+              <select name="uf" id="uf" value={selectedUf} onChange={handleSelectUf} required>
+                <option value="0">Selecione o Estado</option>
+                {ufs.map(uf => (
+                  <option key={uf} value={uf}>{uf}</option>
+                ))}
               </select>
             </div>
 
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" disabled>
-                <option value="">Selecione a cidade</option>
+              <select name="city" id="city" onChange={handleSelectCity} value={selectedCity} required>
+                <option value="0">Selecione a cidade</option>
+                {cities.map(city => (
+                  <option key={city} value={city}>{city}</option>
+                ))}
               </select>
             </div>
           </div>
@@ -77,27 +167,27 @@ const CreatePoint = () => {
             <span>Selecione um ou mais ítens de coleta</span>
           </legend>
           <ul className="items-grid">
-            <li>
+            <li onClick={handleSelectItem}>
               <img src={oleo} alt="oleo" />
               <span>Óleo de cozinha</span>
             </li>
-            <li className="selected">
+            <li className="selected" onClick={handleSelectItem}>
               <img src={baterias} alt="baterias" />
               <span>Pilhas e Baterias</span>
             </li>
-            <li>
+            <li onClick={handleSelectItem}>
               <img src={eletronicos} alt="eletronicos" />
               <span>Eletrônicos</span>
             </li>
-            <li>
+            <li onClick={handleSelectItem}>
               <img src={organicos} alt="organicos" />
               <span>Resíduos Orgânicos</span>
             </li>
-            <li>
+            <li onClick={handleSelectItem}>
               <img src={lampadas} alt="lampadas" />
               <span>Lâmpadas</span>
             </li>
-            <li>
+            <li onClick={handleSelectItem}>
               <img src={papeis} alt="papeis e papelão" />
               <span>Papel e Papelão</span>
             </li>
